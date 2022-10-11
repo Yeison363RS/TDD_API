d80fc09aeba796d81962ca57b75c2fe33c9342bb
Yeison363RS <yehison363@gmail.com>
2022-10-11 11:00:15 -0500

    creacion api

---
+++ src/test/java/com/example/api_hoobies/ApiHoobiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_hoobies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHoobiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
